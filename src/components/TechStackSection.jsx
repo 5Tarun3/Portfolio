b0cb55e3942386e2d6849b33e06267aefd165a5f
5tarun3 <tarun.devrath@gmail.com>
2025-07-26 23:14:17 +0530

    Final changes- with frameworks

--- src/components/TechStackSection.jsx
+++ src/components/TechStackSection.jsx
@@ -10,7 +10,24 @@ import r from '../assets/tech/r.svg';
 import matlab from '../assets/tech/matlab.png';
 import asm from '../assets/tech/assembly.svg';
 import sql from '../assets/tech/sqlite.svg';
-// ...frameworks icons: add to /assets/tech
+import mongodb from '../assets/tech/mongodb.svg';
+import expressjs from '../assets/tech/express.svg';
+import nodejs from '../assets/tech/nodedotjs.svg';
+import reactIcon from '../assets/tech/react.svg';
+import electronjs from '../assets/tech/electron.svg';
+import axios from '../assets/tech/axios.svg';
+import postman from '../assets/tech/postman.svg';
+import numpy from '../assets/tech/numpy.svg';
+import pandas from '../assets/tech/pandas.svg';
+import tableau from '../assets/tech/tableau.png';
+import tensorflow from '../assets/tech/tensorflow.svg';
+import pytorch from '../assets/tech/pytorch.svg';
+import opencv from '../assets/tech/opencv.svg';
+import sklearn from '../assets/tech/scikitlearn.svg';
+import socketio from '../assets/tech/socketdotio.svg';
+import libgdx from '../assets/tech/libgdx.png';
+import figma from '../assets/tech/figma.svg';
+import linux from '../assets/tech/linux.svg';
 
 const languages = [
   {name:"Python", icon:python}, {name:"C", icon:c}, {name:"C++", icon:cpp},
@@ -20,8 +37,24 @@ const languages = [
 ];
 
 const frameworks = [
-  // Add your technology icons similarly here, e.g.
-  // {name: "React", icon:reactIcon}
+  {name: "MongoDB", icon: mongodb},
+  {name: "ExpressJS", icon: expressjs},
+  {name: "NodeJS", icon: nodejs},
+  {name: "React", icon: reactIcon},
+  {name: "ElectronJS", icon: electronjs},
+  {name: "Axios", icon: axios},
+  {name: "Postman", icon: postman},
+  {name: "Numpy", icon: numpy},
+  {name: "Pandas", icon: pandas},
+  {name: "Tableau", icon: tableau},
+  {name: "Tensorflow", icon: tensorflow},
+  {name: "pyTorch", icon: pytorch},
+  {name: "OpenCV", icon: opencv},
+  {name: "Sklearn", icon: sklearn},
+  {name: "Socket.io", icon: socketio},
+  {name: "LibGDX", icon: libgdx},
+  {name: "Figma", icon: figma},
+  {name: "Linux", icon: linux}
 ];
 
 export default function TechStackSection() {
@@ -39,7 +72,9 @@ export default function TechStackSection() {
       <div className="stack-category">
         <h4>Frameworks & Technologies</h4>
         <div className="tech-icons">
-          {/* Repeat for frameworks */}
+          {frameworks.map((item, idx) =>
+            <img src={item.icon} alt={item.name} title={item.name} key={idx} className="tech-icon" />
+          )}
         </div>
       </div>
     </section>
